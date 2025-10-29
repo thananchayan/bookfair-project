c14e502ddab65fe4d78a228cff1f9b6a86ed1843
Ahamed Shahmi <124166029+shahmi0519@users.noreply.github.com>
2025-10-29 14:17:34 +0530

    Vendor Entity is creates

---
+++ backend/vendor-service/src/main/java/com/bookfair/vendor_service/entity/Vendor.java
@@ -0,0 +1,22 @@
+package com.bookfair.vendor_service.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "vendors")
+public class Vendor {
+
+}
