778ea1109115156a9bbaa192b72ab2e8f23a57b3
Ahamed Shahmi <124166029+shahmi0519@users.noreply.github.com>
2025-11-15 23:47:00 +0530

    Enable feignAccess

--- backend/notification-service/src/main/java/com/bookfair/notification_service/NotificationServiceApplication.java
+++ backend/notification-service/src/main/java/com/bookfair/notification_service/NotificationServiceApplication.java
@@ -3,6 +3,7 @@ package com.bookfair.notification_service;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@EnableFeignClients
 @SpringBootApplication
 public class NotificationServiceApplication {
 
