079862b5c628ed47ca9d8bb804376ad7089596bc
Tharmapalan Nevaashahan <79234817+Nevaashahan@users.noreply.github.com>
2025-11-19 09:52:20 +0530

    changes for reservation override

--- backend/stall-service/src/main/java/com/bookfair/stall_service/repository/StallAllocationRepository.java
+++ backend/stall-service/src/main/java/com/bookfair/stall_service/repository/StallAllocationRepository.java
@@ -9,23 +9,25 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface StallAllocationRepository extends JpaRepository<StallAllocationEntity, Long> {
 
-  boolean existsByStall_Id(Long stallId);
+    boolean existsByStall_Id(Long stallId);
 
-  boolean existsByHallStall_IdAndStall_Id(Long hallStallId, Long stallId);
+    boolean existsByHallStall_IdAndStall_Id(Long hallStallId, Long stallId);
 
-  List<StallAllocationEntity> findByBookFair_Id(Long bookFairId);
+    List<StallAllocationEntity> findByBookFair_Id(Long bookFairId);
 
-  boolean existsByBookFair_Id(Long bookFairId);
+    List<StallAllocationEntity> findByBookingUserId(Long userId);
 
-  boolean existsByHallStall_Id(Long hallStallID);
+    boolean existsByBookFair_Id(Long bookFairId);
 
-  List<StallAllocationEntity> findByBookFair_IdAndStallAllocationStatusIn(Long bookFairId,
-      Collection<StallAllocationStatus> statuses
-  );
+    boolean existsByHallStall_Id(Long hallStallID);
 
-  boolean existsByStall_IdAndBookFair_Id(@NotNull(message = "Stall ID is required") Long stallId,
-      @NotNull(message = "Book Fair ID is required") Long bookFairId);
+    List<StallAllocationEntity> findByBookFair_IdAndStallAllocationStatusIn(Long bookFairId,
+                                                                            Collection<StallAllocationStatus> statuses
+    );
+
+    boolean existsByStall_IdAndBookFair_Id(@NotNull(message = "Stall ID is required") Long stallId,
+                                           @NotNull(message = "Book Fair ID is required") Long bookFairId);
 
 
-  List<StallAllocationEntity> findByReservationToken(String token);
+    List<StallAllocationEntity> findByReservationToken(String token);
 }
